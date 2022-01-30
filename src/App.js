3dda676ecb328a11ee36084d2776b693956a86b9
kramstyles <mark.eke@decagon.dev>
2022-01-30 19:41:48 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import react from 'react'
+
+export default function App(){
+    return (
+        <h3>App is working</h3>
+    )
+}
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import react from "react";
+import root from "react-dom"
+import App from "./App"
+
+root.render(<App/>, document.getElementById('root'))
+
