e807dfed8aa358cea7c4dd994b6eb4b5566cf874
kramstyles <mark.eke@decagon.dev>
2022-01-31 21:32:43 +0100

    added hero section inside header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,6 +6,13 @@ function Header(){
                 <img src="./favicon.ico" alt="Airbnb Logo"/>
                 <h2>airbnb</h2>
             </nav>
+            <div className="hero">
+                <img src="./Imgs/hero.png" alt="Hero Image" width="50%" />
+                <div className="hero_content">
+                    <h2>Online Experience</h2>
+                    <p>Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
+                </div>
+            </div>
         </header>
     )
 }
