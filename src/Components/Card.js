376fd8202252ce6a6a77ab1346a2e0cf1553297c
kramstyles <mark.eke@decagon.dev>
2022-02-02 09:46:18 +0100

    Added components from function

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -1,14 +1,34 @@
 import react from 'react'
+//
+// const card_details = [
+//     [5.0, 'USA', 136],
+//     [4.5, 'NIG', 250],
+//     [4.8, 'UK', 120]
+// ]
+// let cards;
+//
+// for (let i = 0; i < card_details.length; i++) {
+//     cards += `<`
+// }
+
+function CardInfo(props) {
+    return (
+        <div className="card">
+            <div className= {props.backImg}/>
+            <a href="#" className="featured_button">sold out</a>
+            <i className="fa fa-star"> </i> <span>{props.star} (6) {props.country}</span>
+            <p>{props.desc}</p>
+            <a href="#"><b>From ${props.price} </b>/ Person</a>
+        </div>
+    )
+}
+
 export default function Card() {
     return (
         <section className="card-holder">
-            <div className="card">
-                <div className="card-img img-1"></div>
-                <a href="#" className="featured_button">sold out</a>
-                <i className="fa fa-star"> </i> <span>5.0 (6) USA</span>
-                <p>Visit our Wonderful Museum</p>
-                <a href="#"><b>From $136 </b>/ Person</a>
-            </div>
+            <CardInfo star="5.0" country="USA" price="136" desc="Visit our Wonderful Museum" backImg="card-img img-1"/>
+            <CardInfo star="4.5" country="NIG" price="212" desc="A state of Culture" backImg="card-img img-2"/>
+            <CardInfo star="4.8" country="UK" price="188" desc="Welcome to United Kingdoms" backImg="card-img img-3"/>
         </section>
     )
 }
