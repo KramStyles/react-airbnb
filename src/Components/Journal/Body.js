83f6f08787e47f8ba70ab4d9249421f0850dfc07
kramstyles <mark.eke@decagon.dev>
2022-02-04 20:18:32 +0100

    fixed .journey for article

--- src/Components/Journal/Body.js
+++ src/Components/Journal/Body.js
@@ -13,6 +13,18 @@ function Body(){
                     </article>
                 </div>
             </div>
+
+            <div className="journey">
+                <div className="img beijing"/>
+                <div className="content">
+                    <article>
+                        <h3><i className="fa fa-map-marker-alt"/> Beijing</h3>
+                        <h1>Mount Fuji</h1>
+                        <b>June - July 2021</b>
+                        <p>Beijing is the capital of the People's Republic of China. It is the world's most populous national capital city, with over 21 million residents within an administrative area of 16,410.5 km2 (6336 sq. mi.).It is located in Northern China, and is governed as a municipality under the direct administration of the State Council with 16 urban, suburban, and rural districts.</p>
+                    </article>
+                </div>
+            </div>
         </section>
     )
 }
