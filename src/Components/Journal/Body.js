be4def3ed0263f408312e341bd64106a7a92c737
kramstyles <mark.eke@decagon.dev>
2022-02-04 21:07:10 +0100

    fixed articles using keys method

--- src/Components/Journal/Body.js
+++ src/Components/Journal/Body.js
@@ -6,12 +6,12 @@ import Data from "./Data";
 function Body(){
     const articles = Data.map(item =>{
         return (
-            // <Components key={item.id} item={item}/>
-            <Components country={item['country']}
-                        place={item['place']}
-                        description={item['description']}
-                        time_period={item['time_period']}
-            />
+            <Components key={item.id} item={item}/>
+            // <Components country={item['country']}
+            //             place={item['place']}
+            //             description={item['description']}
+            //             time_period={item['time_period']}
+            // />
         )
     })
     return(
--- src/Components/Journal/Components.js
+++ src/Components/Journal/Components.js
@@ -4,13 +4,13 @@ import React from "react";
 export default function Article(props){
     return(
         <div className="journey">
-            <div className="img beijing"/>
+            <div className={`img ${props.item.place.toLowerCase()}`} />
             <div className="content">
                 <article>
-                    <h3><i className="fa fa-map-marker-alt"/> {props.country}</h3>
-                    <h1>{props.place}</h1>
-                    <b>{props.time_period}</b>
-                    <p>{props.description}</p>
+                    <h3><i className="fa fa-map-marker-alt"/> {props.item.country}</h3>
+                    <h1>{props.item.place}</h1>
+                    <b>{props.item.time_period}</b>
+                    <p>{props.item.description}</p>
                 </article>
             </div>
         </div>
