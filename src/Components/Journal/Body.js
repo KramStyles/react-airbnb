b19fc0b8559bcdb42f8c61370f79da80fbdf2122
kramstyles <mark.eke@decagon.dev>
2022-02-04 19:48:54 +0100

    working on the individual contents

--- src/Components/Journal/Body.js
+++ src/Components/Journal/Body.js
@@ -1,7 +1,17 @@
 import React from "react";
 function Body(){
     return(
-        <h1>Body</h1>
+        <section>
+            <div className="journey">
+                <div className="img beijing"/>
+                <div className="content">
+                    <h4><i className="fa fa-pointer"/> Beijing</h4>
+                    <h1>Mount Fuji</h1>
+                    <h5>June - July 2021</h5>
+                    <p>Beijing is the capital of the People's Republic of China. It is the world's most populous national capital city, with over 21 million residents within an administrative area of 16,410.5 km2 (6336 sq. mi.).It is located in Northern China, and is governed as a municipality under the direct administration of the State Council with 16 urban, suburban, and rural districts.</p>
+                </div>
+            </div>
+        </section>
     )
 }
 
